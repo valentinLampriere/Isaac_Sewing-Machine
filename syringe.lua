362a8f3acf4cf61792515dcf9b0e7806f68bfff6
valentinLampriere <valentin.lampriere1@gmail.com>
2020-12-28 22:22:04 +0100

    Add loneliness syringe

--- syringe.lua
+++ syringe.lua
@@ -2,6 +2,8 @@ sewingMachineMod.sewnSyringes = {}
 sewnSyringes = sewingMachineMod.sewnSyringes
 
 M_SYR.TOT_SYR.familiarBuff = M_SYR.GetSyringeIdByName("Familiar Buff")
+M_SYR.TOT_SYR.loneliness = M_SYR.GetSyringeIdByName("Loneliness")
+
 
 sewnSyringes.data = {}
 
@@ -13,6 +15,7 @@ function sewnSyringes:addSyringe(id, type, name, description, EIDDescription, du
 		Description = description,
 		EIDDescription = EIDDescription,
 		Duration = duration,
+		Counterpart = counterpart;
 		Weight = weight,
 		Effect = effect,
 		OnUse = onuse,
@@ -33,7 +36,7 @@ function sewnSyringes.familiarsBuff_onUse(idx)
 		local fData = familiar:GetData()
 		familiar = familiar:ToFamiliar()
 		-- Check if the familiar is from the current player
-		if GetPtrHash(familiar.Player) == GetPtrHash(player) then
+		if sewingMachineMod.availableFamiliar[familiar.Variant] ~= nil and GetPtrHash(familiar.Player) == GetPtrHash(player) then
 			-- Save the original level
 			sData.familiarsLevels[GetPtrHash(familiar)] = fData.Sewn_upgradeState or 0
 			
@@ -56,7 +59,7 @@ function sewnSyringes.familiarsBuff_post(player)
 		local fData = familiar:GetData()
 		familiar = familiar:ToFamiliar()
 		-- Check if the familiar is from the current player
-		if GetPtrHash(familiar.Player) == GetPtrHash(player) then
+		if sewingMachineMod.availableFamiliar[familiar.Variant] ~= nil and GetPtrHash(familiar.Player) == GetPtrHash(player) then
 			-- If there is a saved level for the familiar
 			if sData.familiarsLevels[GetPtrHash(familiar)] then
 				-- Set the level of the familiar depending on the saved one
@@ -76,11 +79,69 @@ sewnSyringes:addSyringe(
 	"Upgrade familiars", 
 	"Upgrade your familiar from Normal to Super or Super to Ultra",
 	45 * 30,
-	nil,
+	M_SYR.TOT_SYR.loneliness,
 	1.1,
 	{},
 	sewnSyringes.familiarsBuff_onUse,
 	sewnSyringes.familiarsBuff_post,
+	{familiarsLevels = {}}
+)
+
+
+----------------
+-- LONELINESS --
+----------------
+function sewnSyringes.loneliness_onUse(idx)
+	local sData = sewnSyringes.data[M_SYR.TOT_SYR.loneliness]
+	local player = M_SYR.GetPlayerUsingActive()
+	-- Loop through familiars
+	for _, familiar in pairs(Isaac.FindByType(EntityType.ENTITY_FAMILIAR, -1, -1, false, false)) do
+		local fData = familiar:GetData()
+		familiar = familiar:ToFamiliar()
+		-- Check if the familiar is from the current player
+		if GetPtrHash(familiar.Player) == GetPtrHash(player) then
+			-- Save the original level
+			sData.familiarsLevels[GetPtrHash(familiar)] = fData.Sewn_upgradeState or 0
+			
+			sewingMachineMod:resetFamiliarData(familiar)
+
+			-- Set the familiar's level to 0 (NORMAL, no upgrade)
+			fData.Sewn_upgradeState = 0
+		end
+	end
+end
+function sewnSyringes.loneliness_post(player)
+	local sData = sewnSyringes.data[M_SYR.TOT_SYR.loneliness]
+	-- Loop through familiars
+	for _, familiar in pairs(Isaac.FindByType(EntityType.ENTITY_FAMILIAR, -1, -1, false, false)) do
+		local fData = familiar:GetData()
+		familiar = familiar:ToFamiliar()
+		-- Check if the familiar is from the current player
+		if GetPtrHash(familiar.Player) == GetPtrHash(player) then
+			-- If there is a saved level for the familiar
+			if sData.familiarsLevels[GetPtrHash(familiar)] then
+				-- Set the level of the familiar depending on the saved one
+				fData.Sewn_upgradeState = sData.familiarsLevels[GetPtrHash(familiar)]
+				
+				-- Upgrade the familiar
+				sewingMachineMod:resetFamiliarData(familiar)
+				sewingMachineMod:callFamiliarUpgrade(familiar)
+			end
+		end
+	end
+end
+sewnSyringes:addSyringe(
+	M_SYR.TOT_SYR.loneliness,
+	M_SYR.TYPES.Negative,
+	"Loneliness",
+	"Crownless familiars", 
+	"Familiars lose their crowns and their abilities",
+	60 * 30,
+	M_SYR.TOT_SYR.familiarBuff,
+	1.1,
+	{},
+	sewnSyringes.loneliness_onUse,
+	sewnSyringes.loneliness_post,
 	{familiarsLevels = {}}
 )
 
