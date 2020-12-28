c217b3153de7ec2b62d81f0e7fed87e28d16da0a
valentinLampriere <valentin.lampriere1@gmail.com>
2020-12-28 23:52:52 +0100

    Loop function

--- syringe.lua
+++ syringe.lua
@@ -25,52 +25,55 @@ function sewnSyringes:addSyringe(id, type, name, description, EIDDescription, du
 	sewnSyringes.data[id] = data
 end
 
+function sewnSyringes:loopThroughFamiliars(_function)
+	local player = M_SYR.GetPlayerUsingActive()
+	-- Loop through familiars
+	for _, familiar in pairs(Isaac.FindByType(EntityType.ENTITY_FAMILIAR, -1, -1, false, false)) do
+		familiar = familiar:ToFamiliar()
+		-- Check if the familiar is from the current player
+		if sewingMachineMod.availableFamiliar[familiar.Variant] ~= nil and GetPtrHash(familiar.Player) == GetPtrHash(player) then
+			_function(familiar)
+		end
+	end
+end
+
 --------------------
 -- FAMILIARS BUFF --
 --------------------
 function sewnSyringes.familiarsBuff_onUse(idx)
 	local sData = sewnSyringes.data[M_SYR.TOT_SYR.familiarBuff]
-	local player = M_SYR.GetPlayerUsingActive()
-	-- Loop through familiars
-	for _, familiar in pairs(Isaac.FindByType(EntityType.ENTITY_FAMILIAR, -1, -1, false, false)) do
+
+	sewnSyringes:loopThroughFamiliars(function(familiar)
 		local fData = familiar:GetData()
-		familiar = familiar:ToFamiliar()
-		-- Check if the familiar is from the current player
-		if sewingMachineMod.availableFamiliar[familiar.Variant] ~= nil and GetPtrHash(familiar.Player) == GetPtrHash(player) then
-			-- Save the original level
-			sData.familiarsLevels[GetPtrHash(familiar)] = fData.Sewn_upgradeState or 0
-			
-			sewingMachineMod:resetFamiliarData(familiar, {"Sewn_upgradeState_temporary"})
+		-- Save the original level
+		sData.familiarsLevels[GetPtrHash(familiar)] = fData.Sewn_upgradeState or 0
+				
+		sewingMachineMod:resetFamiliarData(familiar, {"Sewn_upgradeState_temporary"})
 
-			-- Set the familiar's level (unless it is already ultra)
-			if fData.Sewn_upgradeState ~= sewingMachineMod.UpgradeState.ULTRA then
-				fData.Sewn_upgradeState = fData.Sewn_upgradeState + 1
-			end
-
-			-- Upgrade the familiar
-			sewingMachineMod:callFamiliarUpgrade(familiar)
+		-- Set the familiar's level (unless it is already ultra)
+		if fData.Sewn_upgradeState ~= sewingMachineMod.UpgradeState.ULTRA then
+			fData.Sewn_upgradeState = fData.Sewn_upgradeState + 1
 		end
-	end
+
+		-- Upgrade the familiar
+		sewingMachineMod:callFamiliarUpgrade(familiar)
+	end)
 end
 function sewnSyringes.familiarsBuff_post(player)
 	local sData = sewnSyringes.data[M_SYR.TOT_SYR.familiarBuff]
-	-- Loop through familiars
-	for _, familiar in pairs(Isaac.FindByType(EntityType.ENTITY_FAMILIAR, -1, -1, false, false)) do
+
+	sewnSyringes:loopThroughFamiliars(function(familiar)
 		local fData = familiar:GetData()
-		familiar = familiar:ToFamiliar()
-		-- Check if the familiar is from the current player
-		if sewingMachineMod.availableFamiliar[familiar.Variant] ~= nil and GetPtrHash(familiar.Player) == GetPtrHash(player) then
-			-- If there is a saved level for the familiar
-			if sData.familiarsLevels[GetPtrHash(familiar)] then
-				-- Set the level of the familiar depending on the saved one
-				fData.Sewn_upgradeState = sData.familiarsLevels[GetPtrHash(familiar)]
-				
-				-- Upgrade the familiar
-				sewingMachineMod:resetFamiliarData(familiar, {"Sewn_upgradeState_temporary"})
-				sewingMachineMod:callFamiliarUpgrade(familiar)
-			end
+		-- If there is a saved level for the familiar
+		if sData.familiarsLevels[GetPtrHash(familiar)] then
+			-- Set the level of the familiar depending on the saved one
+			fData.Sewn_upgradeState = sData.familiarsLevels[GetPtrHash(familiar)]
+			
+			-- Upgrade the familiar
+			sewingMachineMod:resetFamiliarData(familiar, {"Sewn_upgradeState_temporary"})
+			sewingMachineMod:callFamiliarUpgrade(familiar)
 		end
-	end
+	end)
 end
 sewnSyringes:addSyringe(
 	M_SYR.TOT_SYR.familiarBuff,
@@ -93,29 +96,24 @@ sewnSyringes:addSyringe(
 ----------------
 function sewnSyringes.loneliness_onUse(idx)
 	local sData = sewnSyringes.data[M_SYR.TOT_SYR.loneliness]
-	local player = M_SYR.GetPlayerUsingActive()
-	-- Loop through familiars
-	for _, familiar in pairs(Isaac.FindByType(EntityType.ENTITY_FAMILIAR, -1, -1, false, false)) do
-		local fData = familiar:GetData()
-		familiar = familiar:ToFamiliar()
-		-- Check if the familiar is from the current player
-		if GetPtrHash(familiar.Player) == GetPtrHash(player) then
-			-- Save the original level
-			sData.familiarsLevels[GetPtrHash(familiar)] = fData.Sewn_upgradeState or 0
-			
-			sewingMachineMod:resetFamiliarData(familiar)
 
-			-- Set the familiar's level to 0 (NORMAL, no upgrade)
-			fData.Sewn_upgradeState = 0
-		end
-	end
+	sewnSyringes:loopThroughFamiliars(function(familiar)
+		local fData = familiar:GetData()
+		-- Save the original level
+		sData.familiarsLevels[GetPtrHash(familiar)] = fData.Sewn_upgradeState or 0
+			
+		sewingMachineMod:resetFamiliarData(familiar)
+
+		-- Set the familiar's level to 0 (NORMAL, no upgrade)
+		fData.Sewn_upgradeState = 0
+	end)
 end
 function sewnSyringes.loneliness_post(player)
 	local sData = sewnSyringes.data[M_SYR.TOT_SYR.loneliness]
-	-- Loop through familiars
-	for _, familiar in pairs(Isaac.FindByType(EntityType.ENTITY_FAMILIAR, -1, -1, false, false)) do
+
+	
+	sewnSyringes:loopThroughFamiliars(function(familiar)
 		local fData = familiar:GetData()
-		familiar = familiar:ToFamiliar()
 		-- Check if the familiar is from the current player
 		if GetPtrHash(familiar.Player) == GetPtrHash(player) then
 			-- If there is a saved level for the familiar
@@ -128,7 +126,7 @@ function sewnSyringes.loneliness_post(player)
 				sewingMachineMod:callFamiliarUpgrade(familiar)
 			end
 		end
-	end
+	end)
 end
 sewnSyringes:addSyringe(
 	M_SYR.TOT_SYR.loneliness,
