c5d42e9efef0240ac730667aa1dc8bcd6129385b
valentinLampriere <valentin.lampriere1@gmail.com>
2021-03-07 23:55:10 +0100

    Split strings to fit the paper

--- sewn_scripts/encyclopedia.lua
+++ sewn_scripts/encyclopedia.lua
@@ -1,4 +1,5 @@
 local availableFamiliarId = {}
+local encyclopediaLoaded = false
 
 local function loadAvailableFamiliarId()
     for familiarVariant, familiarData in pairs(sewingMachineMod.availableFamiliar) do
@@ -6,22 +7,42 @@ local function loadAvailableFamiliarId()
     end
 end
 function sewingMachineMod:SetEncyclopedia()
+    if encyclopediaLoaded == true then return end
+    loadAvailableFamiliarId()
     for i, item in pairs(Encyclopedia["itemsTable"].all) do
         if availableFamiliarId[item.ItemId] ~= nil then
-            sewingMachineMod:AddUpgradeToEncyclopedia(item.ItemId, item.WikiDesc)
+            sewingMachineMod:AddUpgradeToEncyclopedia(item.ItemId, item)
         end
     end
+    encyclopediaLoaded = true
 end
-function sewingMachineMod:AddUpgradeToEncyclopedia(id, itemWiki)
-    local wiki = itemWiki
+function sewingMachineMod:AddUpgradeToEncyclopedia(id, itemTab)
+    local wiki = itemTab.WikiDesc
     if sewingMachineMod.FamiliarsUpgradeDescriptions[availableFamiliarId[id]] == nil then return end
     local upSuper = "SUPER : #" .. sewingMachineMod.FamiliarsUpgradeDescriptions[availableFamiliarId[id]].firstUpgrade
-    local upUltra = "ULTRA : # " .. sewingMachineMod.FamiliarsUpgradeDescriptions[availableFamiliarId[id]].secondUpgrade
+    local upUltra = "ULTRA : #" .. sewingMachineMod.FamiliarsUpgradeDescriptions[availableFamiliarId[id]].secondUpgrade
+    --for _, text in pairs(Encyclopedia.fitTextToWidth(sewingMachineMod.FamiliarsUpgradeDescriptions[availableFamiliarId[id]].firstUpgrade, 1, 140)) do
+    --    upSuper = upSuper .. "#" .. text
+    --end
+    --for _, text in pairs(Encyclopedia.fitTextToWidth(sewingMachineMod.FamiliarsUpgradeDescriptions[availableFamiliarId[id]].secondUpgrade, 1, 140)) do
+    --    upUltra = upUltra ..  "#" .. text
+    --end
+    
+    local wikiUpgrade = Encyclopedia.EIDtoWiki(upSuper .. "# #" .. upUltra, "Upgrade")
+    local _wikiUpgrade = {}
+    for i, text in pairs(wikiUpgrade[1]) do
+        local str = text.str
+        if i == 1 then -- Title
+            table.insert(_wikiUpgrade, {str = str, fsize = 2, clr = 3, halign = 0})
+        else
+            for _, subtext in ipairs(Encyclopedia.fitTextToWidth(str, 1, 140)) do
+                table.insert(_wikiUpgrade, {str = subtext})
+            end
+        end
+    end
 
-    local wikiUpgrade = Encyclopedia.EIDtoWiki(upSuper .. "#" .. upUltra, "Upgrade")
-    table.insert(wiki, wikiUpgrade[1])
+    --local wikiUpgrade = Encyclopedia.EIDtoWiki(upSuper .. "# #" .. upUltra, "Upgrade")
+    table.insert(wiki, _wikiUpgrade)
 end
-
-loadAvailableFamiliarId()
 
 sewingMachineMod.errFamiliars.Error()
