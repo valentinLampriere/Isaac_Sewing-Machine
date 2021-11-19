aa21bea9f7de4521410cad7185912e2b75fa72f1
Valentin Lampriere <valentin.lampriere1@gmail.com>
2021-11-19 15:32:19 +0100

    Up bloodshot eye

--- sewn_scripts/entities/familiar/upgrades/prepare_vanilla_familiars.lua
+++ sewn_scripts/entities/familiar/upgrades/prepare_vanilla_familiars.lua
@@ -29,6 +29,7 @@ require("sewn_scripts.entities.familiar.upgrades.vanilla.rainbow_baby")
 require("sewn_scripts.entities.familiar.upgrades.vanilla.lil_gurdy")
 require("sewn_scripts.entities.familiar.upgrades.vanilla.guppy_s_hairball")
 require("sewn_scripts.entities.familiar.upgrades.vanilla.rotten_baby")
+require("sewn_scripts.entities.familiar.upgrades.vanilla.bloodshot_eye")
 
 if REPENTANCE then
     require("sewn_scripts.entities.familiar.upgrades.vanilla.fruity_plum")
---
+++ sewn_scripts/entities/familiar/upgrades/vanilla/bloodshot_eye.lua
@@ -0,0 +1,43 @@
+local Enums = require("sewn_scripts.core.enums")
+
+local BloodshootEye = { }
+
+BloodshootEye.Stats = {
+    TearRotationOffset = 25,
+    LaserTimeout = 1,
+    LaserRadiusMultiplier = 0.01
+}
+
+Sewn_API:MakeFamiliarAvailable(FamiliarVariant.BLOODSHOT_EYE, CollectibleType.COLLECTIBLE_BLOODSHOT_EYE)
+
+Sewn_API:AddFamiliarDescription(
+    FamiliarVariant.BLOODSHOT_EYE,
+    "Fire three tears at once",
+    "Fire a blood laser instead of tears"
+)
+
+function BloodshootEye:OnFamiliarFireTear_Super(familiar, tear)
+    local tData = tear:GetData()
+
+    if tData.Sewn_bloodshotEye_additionalTear ~= true then
+        for i = -1, 1, 2 do
+            local velocity = tear.Velocity:Rotated(BloodshootEye.Stats.TearRotationOffset * i)
+            local newTear = Isaac.Spawn(tear.Type, tear.Variant, tear.SubType, tear.Position, velocity, familiar):ToTear()
+            newTear.CollisionDamage = tear.CollisionDamage
+            newTear.FallingSpeed = tear.FallingSpeed
+            newTear.Height = tear.Height
+            newTear.Scale = tear.Scale
+            newTear.TearFlags = tear.TearFlags
+            --sewnFamiliars:toBabyBenderTear(bloodshotEye, addTear)
+            newTear:GetData().Sewn_bloodshotEye_additionalTear = true
+        end
+    end
+end
+function BloodshootEye:OnFamiliarFireTear_Ultra(familiar, tear)
+    local laser = EntityLaser.ShootAngle(Enums.LaserVariant.LASER_BRIMSTONE, familiar.Position, tear.Velocity:GetAngleDegrees(), BloodshootEye.Stats.LaserTimeout, Vector(0,-20), familiar):ToLaser()
+    tear:Remove()
+    laser.CollisionDamage = tear.CollisionDamage
+    laser.Radius = laser.Radius * BloodshootEye.Stats.LaserRadiusMultiplier
+end
+Sewn_API:AddCallback(Sewn_API.Enums.ModCallbacks.POST_FAMILIAR_FIRE_TEAR, BloodshootEye.OnFamiliarFireTear_Super, FamiliarVariant.BLOODSHOT_EYE, Sewn_API.Enums.FamiliarLevelFlag.FLAG_SUPER)
+Sewn_API:AddCallback(Sewn_API.Enums.ModCallbacks.POST_FAMILIAR_FIRE_TEAR, BloodshootEye.OnFamiliarFireTear_Ultra, FamiliarVariant.BLOODSHOT_EYE, Sewn_API.Enums.FamiliarLevelFlag.FLAG_ULTRA)
