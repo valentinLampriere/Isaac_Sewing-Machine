061944695547197cbdbb5cd83bd7e72468d2fd91
Valentin Lampriere <valentin.lampriere1@gmail.com>
2022-01-06 16:52:19 +0100

    Fix PostPlayerTouchMachine callback

--- sewn_scripts/callbacks/custom/handlers/post_player_touch_machine_handler.lua
+++ sewn_scripts/callbacks/custom/handlers/post_player_touch_machine_handler.lua
@@ -8,11 +8,11 @@ PostPlayerTouchMachineHandler.ID = Enums.ModCallbacks.POST_PLAYER_TOUCH_MACHINE
 PostPlayerTouchMachineHandler.DefaultArguments = { -1 }
 
 local function OnMachineUpdate(_, machine)
-    for _, callback in ipairs(PostPlayerTouchMachineHandler.RegisteredCallbacks) do
-        if callback.Argument[1] == PostPlayerTouchMachineHandler.DefaultArguments[1] or machine.Variant == callback.Argument[1] then
-            for i = 1, Globals.Game:GetNumPlayers() do
-                local player = Isaac.GetPlayer(i - 1)
-                if (machine.Position - player.Position):LengthSquared() < (machine.Size + player.Size) ^ 2 then
+    for i = 1, Globals.Game:GetNumPlayers() do
+        local player = Isaac.GetPlayer(i - 1)
+        if (machine.Position - player.Position):LengthSquared() < (machine.Size + player.Size) ^ 2 then
+            for _, callback in ipairs(PostPlayerTouchMachineHandler.RegisteredCallbacks) do
+                if callback.Argument[1] == PostPlayerTouchMachineHandler.DefaultArguments[1] or machine.Variant == callback.Argument[1] then
                     callback:Function(player, machine)
                 end
             end
